dade2ff97872241d6e0ec989b436c8da6c3a2ac6
achahid19 <anasks1999@gmail.com>
2025-01-14 15:44:07 +0100

    configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,39 +3,35 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/achahid19/Profiles-REST-API.git'
+PROJECT_GIT_URL='https://github.com/CHANGEME'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install dependencies
+# Install Python, SQLite and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3.10 python3.10-venv python3.10-dev sqlite3 python3-pip supervisor nginx git build-essential #com
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
-# Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
-# Create a virtual environment with Python 3.11
-python3.11 -m venv $PROJECT_BASE_PATH/env
+python3 -m venv $PROJECT_BASE_PATH/env
 
-# Install Python packages
-$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip setuptools wheel
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.22
+$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to manage the uWSGI process
+# Setup Supervisor to run our uwsgi process.
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup Nginx to make the application accessible
+# Setup nginx to make our application accessible.
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
