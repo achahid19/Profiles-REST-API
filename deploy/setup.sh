9aa3b7af501961a063fb3c90b80841cb2d850c23
achahid19 <anasks1999@gmail.com>
2025-01-14 15:37:47 +0100

    configured for deployment...

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -10,28 +10,32 @@ PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install dependencies
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3.11-dev python3.11-venv sqlite3 python3-pip supervisor nginx git # com
+apt-get install -y python3.11 python3.11-venv python3.11-dev sqlite3 python3-pip supervisor nginx git build-essential
 
+# Create project directory
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
-python3 -m venv $PROJECT_BASE_PATH/env
+# Create a virtual environment with Python 3.11
+python3.11 -m venv $PROJECT_BASE_PATH/env
 
+# Install Python packages
+$PROJECT_BASE_PATH/env/bin/pip install --upgrade pip setuptools wheel
 $PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.22
 
 # Run migrations
 $PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
 
-# Setup Supervisor to run our uwsgi process.
+# Setup Supervisor to manage the uWSGI process
 cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
 supervisorctl reread
 supervisorctl update
 supervisorctl restart profiles_api
 
-# Setup nginx to make our application accessible.
+# Setup Nginx to make the application accessible
 cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
 rm /etc/nginx/sites-enabled/default
 ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
