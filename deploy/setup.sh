45bf99571aa8f73fb80b3558654c0dacfd4572a8
achahid19 <anasks1999@gmail.com>
2025-01-14 15:39:46 +0100

    configured for deployment....

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -13,7 +13,7 @@ locale-gen en_GB.UTF-8
 # Install dependencies
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3.11 python3.11-venv python3.11-dev sqlite3 python3-pip supervisor nginx git build-essential
+apt-get install -y python3.10 python3.10-venv python3.10-dev sqlite3 python3-pip supervisor nginx git build-essential
 
 # Create project directory
 mkdir -p $PROJECT_BASE_PATH
